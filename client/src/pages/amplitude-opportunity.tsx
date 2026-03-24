0c7671baa824442c87db7482ce0c428821c9d156
Aditi Parvati <aditiparvati27@gmail.com>
2026-03-24 13:24:40 -0400

    Add /amplitude/opportunity endpoint for new Amplitude deck

---
+++ client/src/pages/amplitude-opportunity.tsx
@@ -0,0 +1,11 @@
+export default function AmplitudeOpportunity() {
+  return (
+    <main className="min-h-screen bg-background">
+      <iframe
+        title="Amplitude Opportunity"
+        src="/amplitude-opportunity.html"
+        className="h-screen w-full border-0"
+      />
+    </main>
+  );
+}
